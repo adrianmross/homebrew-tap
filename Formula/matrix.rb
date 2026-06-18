22ba5eb6af463b2b8ea501f1152e27ef9d86e55e
Adrian <adrian.ross@oracle.com>
2026-06-18 15:13:27 +0000

    matrix 0.3.3

--- Formula/matrix.rb
+++ Formula/matrix.rb
@@ -1,19 +1,21 @@
 class Matrix < Formula
   desc "Compatibility matrix CLI for software zones, levels, facts, gates, and traces"
   homepage "https://github.com/adrianmross/matrix"
-  url "https://github.com/adrianmross/matrix/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "023b4fa1a2857fd5c9d52e4e0ae86753561cf716fc92827fd6cd6515680403cf"
+  url "https://github.com/adrianmross/matrix/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "2562b416871d2569e12641e980b0d5e766d41e60c686d7373a1dd66a6fc57f59"
   license "Apache-2.0"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args(path: "crates/matrix-enter")
     generate_completions_from_executable(bin/"matrix", "completion")
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/matrix --version")
+    assert_match version.to_s, shell_output("#{bin}/matrix-enter --version")
     assert_match "\"construct\"", shell_output("#{bin}/matrix --construct http://127.0.0.1:1 --json doctor")
     assert_match "_matrix", shell_output("#{bin}/matrix completion zsh")
   end
