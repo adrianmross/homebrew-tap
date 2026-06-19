86ead5a35fb1ba011117b83dcef0bdf86f894896
Adrian <adrian.ross@oracle.com>
2026-06-19 01:24:22 +0000

    matrix 0.3.8

--- Formula/matrix.rb
+++ Formula/matrix.rb
@@ -1,8 +1,8 @@
 class Matrix < Formula
   desc "Compatibility matrix CLI for software zones, levels, facts, gates, and traces"
   homepage "https://github.com/adrianmross/matrix"
-  url "https://github.com/adrianmross/matrix/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "79f4b3444de94d01b68596debbff14715eb9c8cb505a25d4724c0602d4a46d94"
+  url "https://github.com/adrianmross/matrix/archive/refs/tags/v0.3.8.tar.gz"
+  sha256 "28ddfbbe870e033e3b102516a2ea93703e0c2dbb11eb2b3c27b8749306376abb"
   license "Apache-2.0"
 
   depends_on "rust" => :build
@@ -10,12 +10,14 @@ class Matrix < Formula
   def install
     system "cargo", "install", *std_cargo_args
     system "cargo", "install", *std_cargo_args(path: "crates/matrix-enter")
+    system "cargo", "install", *std_cargo_args(path: "crates/matrix-construct")
     generate_completions_from_executable(bin/"matrix", "completion")
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/matrix --version")
     assert_match version.to_s, shell_output("#{bin}/matrix-enter --version")
+    assert_match version.to_s, shell_output("#{bin}/matrix-construct --version")
     assert_match "\"construct\"", shell_output("#{bin}/matrix --construct http://127.0.0.1:1 --json doctor")
     assert_match "_matrix", shell_output("#{bin}/matrix completion zsh")
   end
