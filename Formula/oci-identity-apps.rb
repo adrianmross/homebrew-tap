083ef32f3dfda44cc1aba7365cc7faf2778d89c8
Adrian Ross <adrian.ross@oracle.com>
2026-06-26 21:08:56 -0700

    oci-identity-apps 0.1.0 (#30)

---
+++ Formula/oci-identity-apps.rb
@@ -0,0 +1,27 @@
+class OciIdentityApps < Formula
+  desc "Plan OCI Identity Domains OAuth applications for CLI and automation flows"
+  homepage "https://github.com/adrianmross/oci-identity-apps"
+  url "https://github.com/adrianmross/oci-identity-apps/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9f5b53e8b1a4487e6b63ccd673cf34bf17250d7a0c410b9526b77e977e5ccc86"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/adrianmross/oci-identity-apps/internal/cli.version=#{version}
+      -X github.com/adrianmross/oci-identity-apps/internal/cli.commit=homebrew
+      -X github.com/adrianmross/oci-identity-apps/internal/cli.date=homebrew
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"oci-identity-apps", ldflags:), "./cmd/oci-identity-apps"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/oci-identity-apps version")
+    output = shell_output("#{bin}/oci-identity-apps plan --issuer https://idcs-example.identity.oraclecloud.com --scope https://service.example.com/.default --include user --format json")
+    assert_match "oci-identity-apps.plan.v1", output
+    assert_match "authorization_code", output
+  end
+end
