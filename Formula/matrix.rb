2c5d827591fae893c777fb9fa0bc02aa371c9805
Adrian Ross <adrian.ross@oracle.com>
2026-06-29 00:16:27 -0700

    Update matrix formula to 0.3.25 (#51)

--- Formula/matrix.rb
+++ Formula/matrix.rb
@@ -3,17 +3,17 @@ class Matrix < Formula
   homepage "https://github.com/adrianmross/matrix"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/adrianmross/matrix/releases/download/v0.3.24/matrix-0.3.24-aarch64-apple-darwin.tar.gz"
-      sha256 "789033a2c9aff8ce4151d8543cbcc3425f83c26fcf56de0a056c2bbb6bf119e6"
+      url "https://github.com/adrianmross/matrix/releases/download/v0.3.25/matrix-0.3.25-aarch64-apple-darwin.tar.gz"
+      sha256 "25d88c327b9bab6c90d239f591b358b7bdac8bc60e451ddfff8dab40e8b23316"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/adrianmross/matrix/releases/download/v0.3.24/matrix-0.3.24-x86_64-apple-darwin.tar.gz"
-      sha256 "7379313547ee6e803508138eef648a553a7cc0964fd4f0dc34c47f2fc6ceffdc"
+      url "https://github.com/adrianmross/matrix/releases/download/v0.3.25/matrix-0.3.25-x86_64-apple-darwin.tar.gz"
+      sha256 "ee77bfb84e57c68793c6afcde74ed242cee46967945fed6ef3d1a6c69b65d493"
     end
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/adrianmross/matrix/releases/download/v0.3.24/matrix-0.3.24-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "ffb6754a8e2f6ee93724bb757cdbea5e8921929f4fb0aedbc5dc27141c676b2e"
+    url "https://github.com/adrianmross/matrix/releases/download/v0.3.25/matrix-0.3.25-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "d96a5d78cb8b79d3f937d4a241df65a0088d183938c9ebc65885604ec4b81966"
   end
   license "Apache-2.0"
 
